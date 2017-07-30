26a5f4fbbce47597632dda2f233f5ff44cb06f44
Md. Abu Ismaiel Zabiullah <mdxavi20@gmail.com>
2017-07-30 12:38:16 +0600

    Add files via upload

---
+++ Class-21/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $(".homepage-slider").owlCarousel({
+            items:1,
+            nav:false,
+            dots:true,
+            loop:true
+        })
+
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
